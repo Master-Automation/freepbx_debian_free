25e1de8ad3db402f9d90a7b7c2be330f0e366575
Master Automation <GitHub2025@mail.ru>
2026-04-19 14:53:40 +0300

    Update russian.sh

--- russian.sh
+++ russian.sh
@@ -5,7 +5,7 @@
 # Версия: 3.1 (Полностью рабочая)
 #####################################################################################
 set -e
-SCRIPTVER="3.1"
+SCRIPT_VERSION="3.1"
 ASTVERSION=${ASTVERSION:-22}
 PHPVERSION="8.2"
 LOG_FOLDER="/var/log/pbx"
